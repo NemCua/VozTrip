1ebba871cea880efda044015f551b2bd0d5f2642
NemCua <nemcuaa1@gmail.com>
2026-02-09 11:02:49 +0700

    userModel and userServices

--- app_thuyet_minh_server/Program.cs
+++ app_thuyet_minh_server/Program.cs
@@ -1,6 +1,20 @@
+using Npgsql;
+using app_thuyet_minh_server.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connStr = "Host=localhost;Port=5432;Database=app_thuyet_minh;Username=nguyenquochuy;Password=mypassword;";
+
+
+builder.Services.AddSingleton(new UserService(connStr));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/api/get/user", async (UserService userService) =>
+{
+    return await userService.GetThisUser(1);
+});
 
 app.Run();
+
